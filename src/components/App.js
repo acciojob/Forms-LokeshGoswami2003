ef57000d2828ec506f4726b250f3ed78f4a93269
Lokesh Goswami <lokesh.goswami.2003@gmail.com>
2025-11-05 18:39:38 +0530

    try

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,36 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Form from "./Form";
+import FormRef from "./FormRef";
+import FormState from "./FormState";
 
+export default function App() {
+  return (
+    <Router>
+      <nav
+        style={{
+          display: "flex",
+          gap: 12,
+          justifyContent: "center",
+          marginTop: 20,
+        }}
+      >
+        <Link id="form-link" to="/form">
+          Layout
+        </Link>
+        <Link id="form-ref-link" to="/form-ref">
+          useRef
+        </Link>
+        <Link id="form-state-link" to="/form-state">
+          useState
+        </Link>
+      </nav>
+
+      <Switch>
+        <Route exact path="/form" component={Form} />
+        <Route path="/form-ref" component={FormRef} />
+        <Route path="/form-state" component={FormState} />
+      </Switch>
+    </Router>
+  );
+}
